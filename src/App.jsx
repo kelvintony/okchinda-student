68ed456c54c8520611e4113c8840c9542205a795
liteoftheworld <kosuruth100@gmail.com>
2024-03-21 10:51:00 +0100

    i finished the success page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css';
-import Home from './pages/Home/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Home from "./pages/Home/Home";
+import SuccessMessage from "./components/SuccessMessage/SuccessMessage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route exact path='/' element={<Home />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/successmessage" element={<SuccessMessage />} />
         </Routes>
       </BrowserRouter>
     </>
