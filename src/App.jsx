fee11f87af4ae0a3dec6ada1697c9d19404d0e14
Debby-hub <debbyoziblaze@gmail.com>
2024-03-21 13:26:11 +0100

    I made the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css';
-import Home from './pages/Home/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Login from "./pages/Login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route exact path='/' element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
