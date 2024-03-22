63012beaace5537a7622596e860d17877569dd27
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-03-22 01:38:10 +0100

    designed the homepage and created and also designed the menu components

---
+++ src/components/Data/Data.js
@@ -0,0 +1,124 @@
+import food1 from "../../assets/Ellipse-187.png";
+import food2 from "../../assets/food2.png";
+import food3 from "../../assets/food3.png";
+import food4 from "../../assets/food4.png";
+import food5 from "../../assets/food5.png";
+import egusi from "../../assets/egusi.png";
+import puff from "../../assets/puff.png";
+import swallow1 from "../../assets/pounded.png";
+import swallow2 from "../../assets/eba.png";
+import swallow3 from "../../assets/wheat.png";
+import protein1 from "../../assets/chicken.png";
+import protein2 from "../../assets/turkey.png";
+import protein3 from "../../assets/beef.png";
+
+export const FOOD_DATA = [
+  {
+    id: 1,
+    image: food1,
+    title: "All meals",
+    selected: false,
+  },
+  {
+    id: 2,
+    image: food2,
+    title: "Wines",
+    selected: false,
+  },
+  {
+    id: 3,
+    image: food3,
+    title: "Desserts",
+    selected: false,
+  },
+  {
+    id: 4,
+    image: food4,
+    title: "Soups",
+    selected: false,
+  },
+  {
+    id: 5,
+    image: food5,
+    title: "Chops",
+    selected: false,
+  },
+];
+
+export const MEALS_DATA = [
+  {
+    id: 1,
+    image: egusi,
+    title: "Egusi Soup & Swallow",
+    // selected: true,
+  },
+  {
+    id: 2,
+    image: puff,
+    title: "Small chops",
+  },
+];
+export const SWALLOW_DATA = [
+  {
+    id: 1,
+    image: swallow1,
+    title: "Poundo",
+    selected: false,
+  },
+  {
+    id: 2,
+    image: swallow2,
+    title: "Eba",
+    selected: false,
+  },
+  {
+    id: 3,
+    image: swallow3,
+    title: "Wheat",
+    selected: false,
+  },
+];
+export const PROTEIN_DATA = [
+  {
+    id: 1,
+    image: protein1,
+    title: "Chicken",
+    // selected: true,
+  },
+  {
+    id: 2,
+    image: protein2,
+    title: "Turkey",
+  },
+  {
+    id: 3,
+    image: protein3,
+    title: "Beef",
+  },
+];
+export const INGREDIENTS = [
+  {
+    id: 1,
+    name: "Ground Egusi Seed",
+  },
+  {
+    id: 2,
+    name: "Spinach",
+  },
+  {
+    id: 3,
+    name: "Palm Oil",
+  },
+  {
+    id: 4,
+    name: "Beef",
+  },
+  {
+    id: 5,
+    name: "Stockfish",
+  },
+  {
+    id: 6,
+    name: "Seasoning",
+  },
+];
