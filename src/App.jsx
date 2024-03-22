61afe3c9788e5395c0376f08c5fde7af1fce5105
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-03-22 03:55:51 +0100

    designed event page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Event from "./pages/Event/Event";
 import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/event" element={<Event />} />
         </Routes>
       </BrowserRouter>
     </>
