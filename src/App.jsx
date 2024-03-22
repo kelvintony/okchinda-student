feeb5b51148f8ea606b5eb5ba4fa77e0f0c2b8ab
kelvintony <kelvintonyph@gmail.com>
2024-03-22 16:36:43 +0100

    test 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,48 @@
-
-import "./App.css";
 // import "./App.css";
-import Event from "./pages/Event/Event";
+import Event from './pages/Event/Event';
 // import "./App.css";
-import Home from "./pages/Home/Home";
-import SuccessMessage from "./components/SuccessMessage/SuccessMessage";
-import SelectMealRice from "./pages/SelectMealRice/SelectMealRice";
-import MyCart from "./pages/MyCart/MyCart";
-import MyOrder from "./pages/MyOrder/MyOrder";
+// import Home from "./pages/Home/Home";
+import SuccessMessage from './components/SuccessMessage/SuccessMessage';
+import SelectMealRice from './pages/SelectMealRice/SelectMealRice';
+import MyCart from './pages/MyCart/MyCart';
+import MyOrder from './pages/MyOrder/MyOrder';
 
 // import "./App.css";
-import Login from "./pages/Login/Login";
-import SelectMealSoup from "./pages/SelectMealSoup/SelectMealSoup";
-import EmptyCart from "./pages/EmptyCart/EmptyCart";
-import CartWithOrder from "./pages/CartWithOrder/CartWithOrder";
+import Login from './pages/Login/Login';
+import SelectMealSoup from './pages/SelectMealSoup/SelectMealSoup';
+import EmptyCart from './pages/EmptyCart/EmptyCart';
+import CartWithOrder from './pages/CartWithOrder/CartWithOrder';
 
-import MealPreferenceRice from "./pages/MealPreferenceRice/MealPreferenceRice";
+import MealPreferenceRice from './pages/MealPreferenceRice/MealPreferenceRice';
 
-import Home from "./pages/Home/Home";
+import Home from './pages/Home/Home';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/event" element={<Event />} />
-          <Route exact path="/successmessage" element={<SuccessMessage />} />
-          <Route exact path="/SelectMealRice" element={<SelectMealRice />} />
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/event' element={<Event />} />
+          <Route exact path='/successmessage' element={<SuccessMessage />} />
+          <Route exact path='/SelectMealRice' element={<SelectMealRice />} />
 
-          <Route exact path="/cart" element={<MyCart />} />
-          <Route exact path="/order" element={<MyOrder />} />
+          <Route exact path='/cart' element={<MyCart />} />
+          <Route exact path='/order' element={<MyOrder />} />
 
-          <Route exact path="/login" element={<Login />} />
+          <Route exact path='/login' element={<Login />} />
 
-          <Route exact path="/select-meal-soup" element={<SelectMealSoup />} />
+          <Route exact path='/select-meal-soup' element={<SelectMealSoup />} />
 
-          <Route exact path="/empty-cart" element={<EmptyCart />} />
+          <Route exact path='/empty-cart' element={<EmptyCart />} />
 
-          <Route exact path="/cart-with-order" element={<CartWithOrder />} />
+          <Route exact path='/cart-with-order' element={<CartWithOrder />} />
 
           <Route
             exact
-            path="/meal-preference"
+            path='/meal-preference'
             element={<MealPreferenceRice />}
           />
         </Routes>
