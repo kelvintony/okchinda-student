82ab04e41918486cb096bb8a7a0a862af309888d
liteoftheworld <kosuruth100@gmail.com>
2024-03-22 01:06:45 +0100

    i just finished the rice page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
 import SuccessMessage from "./components/SuccessMessage/SuccessMessage";
+import SelectMealRice from "./pages/SelectMealRice/SelectMealRice";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/successmessage" element={<SuccessMessage />} />
+          <Route exact path="/SelectMealRice" element={<SelectMealRice />} />
         </Routes>
       </BrowserRouter>
     </>
